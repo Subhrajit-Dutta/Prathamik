72da855aa9c524424ccf4669d2b4d5431dccbd3c
Subhrajit <subhrajitdutta007@gmail.com>
2024-08-08 13:31:07 +0530

    update

--- client/src/API.js
+++ client/src/API.js
@@ -1,4 +1,4 @@
-export const authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlrZXkiOiI0NGUwZDAwOC03ZTJhLTRlNzYtYTAwYS1mMGU2NWMzMTNlZGIiLCJwZXJtaXNzaW9ucyI6WyJhbGxvd19qb2luIl0sImlhdCI6MTcxNjk3ODI4MiwiZXhwIjoxNzE3NTgzMDgyfQ.3W2Gox5pYmzJ_VYDRMNxwD22U3KmZACcV1Lexa_u4fM";
+export const authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlrZXkiOiI0NGUwZDAwOC03ZTJhLTRlNzYtYTAwYS1mMGU2NWMzMTNlZGIiLCJwZXJtaXNzaW9ucyI6WyJhbGxvd19qb2luIl0sImlhdCI6MTcyMzEwNDAzOCwiZXhwIjoxNzU0NjQwMDM4fQ.xdLFbVeTsBkzKRgeWNa4mK5EY3k1HblNdSFFovmXI48";
 export const createMeeting = async ({ token }) => {
   const res = await fetch(`https://api.videosdk.live/v2/rooms`, {
     method: "POST",
